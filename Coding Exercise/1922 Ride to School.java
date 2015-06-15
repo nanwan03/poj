c05fdd0fa1023823699c1033a23141cfb4823c6f
unknown <nanwan03@NANWAN03-002251.usa.Arm.com>
2015-06-15 15:52:09 -0700

    commit

---
+++ Coding Exercise/1922 Ride to School.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (true) {
+			int testcase = in.nextInt();
+			if (testcase == 0) {
+				System.exit(-1);
+			}
+			double min = Double.POSITIVE_INFINITY;
+			while (testcase-- > 0) {
+				int v = in.nextInt();
+				int t = in.nextInt();
+				if (t < 0) {
+					continue;
+				}
+				double arriveTime = 4.5 / v * 3600 + t;
+				min = Math.min(min, arriveTime);
+			}
+			System.out.print((int)(Math.ceil(min)) + "\n");
+		}
+	}
+}
