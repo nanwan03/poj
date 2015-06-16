3fc666c2c5a9a3b853f04d0cc4bb933166435fe7
unknown <nanwan03@NANWAN03-002251.usa.Arm.com>
2015-06-16 13:31:58 -0700

    commit

--- Mathematics/2309 BST.java
+++ Mathematics/2309 BST.java
@@ -8,20 +8,8 @@ public class Main {
 		int testcase = Integer.parseInt(in.nextLine());
 		while (testcase-- > 0) {
 			int input = in.nextInt();
-			int level = getRightMostOne(input);
-			int diff = (int)Math.pow(2.0, (double)level) - 1;
+			int diff = (input & (-input)) - 1;
 			System.out.println((input - diff) + " " + (input + diff));
 		}
-	}
-	private static int getRightMostOne(int input) {
-		int idx = 0;
-		while (input > 0) {
-			if ((input & 1) == 1) {
-				return idx;
-			}
-			idx++;
-			input >>= 1;
-		}
-		return idx;
 	}
 }
