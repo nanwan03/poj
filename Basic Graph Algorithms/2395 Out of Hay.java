18451a1211cf9ab5727b281dcf3b17b627b8e47e
unknown <nanwan03@NANWAN03-002251.usa.Arm.com>
2015-07-06 16:28:26 -0700

    commit

---
+++ Basic Graph Algorithms/2395 Out of Hay.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+	private static final int MAX = 1000000001;
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int size = in.nextInt();
+		int lines = in.nextInt();
+		int[][] map = new int[size][size];
+		for (int i = 0; i < size; ++i) {
+			for (int j = 0; j < size; ++j) {
+				map[i][j] = MAX;
+			}
+		}
+		for (int i = 1; i <= lines; ++i) {
+			int u = in.nextInt() - 1;
+			int v = in.nextInt() - 1;
+			int dis = in.nextInt();
+			map[u][v] = Math.min(map[u][v], dis);
+			map[v][u] = Math.min(map[v][u], dis);
+		}
+		System.out.println(prime(map, size));
+	}
+	private static int prime(int[][] map, int size) {
+		int max = 0;
+		int[] lowCost = new int[size];
+		boolean[] used = new boolean[size];
+		for (int i = 0; i < size; ++i) {
+			lowCost[i] = map[0][i];
+			used[i] = false;
+		}
+		used[0] = true;
+		for (int i = 1; i < size; ++i) {
+			int j = 0;
+			while (used[j]) {
+				j++;
+			}
+			for (int k = 0; k < size; ++k) {
+				if ((!used[k]) && (lowCost[k] < lowCost[j])) {
+					j = k;
+				}
+			}
+			used[j] = true;
+			max = Math.max(max, lowCost[j]);
+			for (int k = 0; k < size; ++k) {
+				if (!used[k] && (map[j][k] < lowCost[k])) {
+					lowCost[k] = map[j][k];
+				}
+			}
+		}
+		return max;
+	}
+}
