537b2d7d68c04b32d1fb14c7417298538e1d80c0
nanwan03 <wn1842@gmail.com>
2015-07-11 15:13:25 -0400

    commit

---
+++ Network Flow Problems/1274 The Perfect Stall.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			int cowNumber = in.nextInt();
+			int stallNumber = in.nextInt();
+			boolean[][] map = new boolean[cowNumber + 1][stallNumber + 1];
+			int[] linker = new int[stallNumber + 1];
+			Arrays.fill(linker, -1);
+			for (int i = 1; i <= cowNumber; ++i) {
+				int preferNumber = in.nextInt();
+				for (int j = 0; j < preferNumber; ++j) {
+					int prefer = in.nextInt();
+					map[i][prefer] = true;
+				}
+			}
+			System.out.println(hungary(map, linker, cowNumber, stallNumber));
+		}
+	}
+	private static int hungary(boolean[][] map, int[] linker, int cowNumber, int stallNumber) {
+		int rst = 0;
+		for (int i = 1; i <= cowNumber; ++i) {
+			boolean[] used = new boolean[stallNumber + 1];
+			if (dfs(map, linker, used, cowNumber, stallNumber, i)) {
+				rst++;
+			}
+		}
+		return rst;
+	}
+	private static boolean dfs(boolean[][] map, int[] linker, boolean[] used, int cowNumber, int stallNumber, int cow) {
+		for (int i = 1; i <= stallNumber; ++i) {
+			if (map[cow][i] && !used[i]) {
+				used[i] = true;
+				if (linker[i] == -1 || dfs(map, linker, used, cowNumber, stallNumber, linker[i])) {
+					linker[i] = cow;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
